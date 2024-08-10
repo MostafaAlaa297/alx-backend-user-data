97d37277f7c8d6a1c4645dafb33564aaef56d362
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-10 15:27:12 -0700

    fix: debug app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,26 +3,23 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
-
+from flask_cors import CORS
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Create the auth variable
+
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
-# Load the correct authentication class based on the environment variable
 if AUTH_TYPE == "basic_auth":
-    from api.v1.auth.auth import BasicAuth
     auth = BasicAuth()
 else:
-    from api.v1.auth.basic_auth import Auth
     auth = Auth()
 
 
@@ -41,22 +38,21 @@ def unauthorized(error) -> str:
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler"""
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request():
-    """Handler that runs before each request
-    to filter based on authentication."""
+    """Handler that runs before each request to filter based on authentication."""
     if auth is None:
         return
 
     excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
-            ]
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
