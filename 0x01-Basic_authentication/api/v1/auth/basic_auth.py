497ce15e41d4e5f474000942ba5980b21fc2f88c
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-10 18:39:22 -0700

    fix: pycodestyle in User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,7 +22,7 @@ class BasicAuth(Auth):
             return authorization_header.split("Basic ", 1)[1]
 
     def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
+        self, base64_authorization_header: str) -> str:
         """returns the decoded value of a
         Base64 string base64_authorization_header"""
         if base64_authorization_header is None:
@@ -36,7 +36,7 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
+        self, decoded_base64_authorization_header: str) -> (str, str):
         """returns the user email and password from the Base64 decoded value"""
         if decoded_base64_authorization_header is None:
             return None, None
@@ -49,7 +49,7 @@ class BasicAuth(Auth):
         return email, password
 
     def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """Returns the User instance based on the provided email and password."""
         if user_email is None or not isinstance(user_email, str):
             return None
