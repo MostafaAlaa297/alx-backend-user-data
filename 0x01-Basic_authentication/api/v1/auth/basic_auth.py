dd5b681756039d6882ec5fb06f1db8d754939ccb
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-10 18:38:08 -0700

    fix: pycodestyle in User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 BasicAuth module for the API authentication.
 """
+from models.user import User
 from api.v1.auth.auth import Auth
 import base64
 
@@ -20,7 +21,8 @@ class BasicAuth(Auth):
         else:
             return authorization_header.split("Basic ", 1)[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """returns the decoded value of a
         Base64 string base64_authorization_header"""
         if base64_authorization_header is None:
@@ -33,7 +35,8 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """returns the user email and password from the Base64 decoded value"""
         if decoded_base64_authorization_header is None:
             return None, None
@@ -44,3 +47,22 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(":")
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Returns the User instance based on the provided email and password."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
