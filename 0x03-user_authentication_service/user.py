14be3358fae044f0e483122fec2d8cd65fd60284
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-16 06:29:49 -0700

    create and config sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+User authentication service
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    ___tablename___ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql:///users.db')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
