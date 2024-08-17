20c9112e0bdcc1918888a214e7478ee0687dfc90
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-17 10:28:36 -0700

    finish task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+0x00. Personal data
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    pattern = f'({"|".join(fields)})=.*?{separator}'
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
